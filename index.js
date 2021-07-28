c71360a233dfb9c9bb65f0c8ca198daa1385758d
José <pachajsc@users.noreply.github.com>
2021-07-28 19:36:52 -0300

    header

--- index.js
+++ index.js
@@ -59,10 +59,7 @@ app.get( '/items', ( request, response ) =>
 
 app.post( '/items', ( request, response ) =>
 {
-    response.header("Access-Control-Allow-Origin", true);
-  response.header("Access-Control-Allow-Credentials", true);
-  response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  response.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+   
     const item = request.body
     if ( !item.content )
     {
@@ -83,7 +80,7 @@ app.post( '/items', ( request, response ) =>
         date: new Date().toISOString()
     }
     items = [ ...items, newItem]
-    response.json( newItem)
+    response.json(newItem)
 } )
 
 
