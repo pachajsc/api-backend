bc2594c8de8a54a4512ff2d42e4e9b55a94b1a93
José <pachajsc@users.noreply.github.com>
2021-07-28 15:14:04 -0300

    header

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.get( '/items', ( request, response ) =>
 
 app.post( '/items', ( request, response ) =>
 {
-    response.header("Access-Control-Allow-Origin", '*');
+    response.header("Access-Control-Allow-Origin", true);
   response.header("Access-Control-Allow-Credentials", true);
   response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   response.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
