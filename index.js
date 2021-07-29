665ab6e498e3de0ad4e0d52ff1e22f4649f1f1e7
José <pachajsc@users.noreply.github.com>
2021-07-29 14:19:34 -0300

    cors

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require( 'express' )
-const cors = require('cors')
+const cors = require('express')
 const { createProxyMiddleware } = require( 'http-proxy-middleware' );
 const app = express()
 
 //app.use(cors())
-const WhiteList =['https://qurvi.com.ar'];
+const WhiteList ='https://qurvi.com.ar';
 app.use(cors({origin:WhiteList}));
 app.use( express.json() )
 //app.use( logger)
