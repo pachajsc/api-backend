5b69473aa5d5d35614e9488cabbb1a89e0f5cc14
José <pachajsc@users.noreply.github.com>
2021-08-06 15:00:28 -0300

    Update index.js

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ app.delete("/items/:id", (request, response) => {
 
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-const envPath = process.env.NODE_ENV !== 'production' ? `.env.${process.env.NODE_ENV}` : '.env';
+const envPath = process.env.NODE_ENV !== 'production' ? `.env.${process.env.NODE_ENV}` : 'https://qr-app-2021.herokuapp.com';
 const config = require('dotenv').config({path: envPath});
 
 port = process.env.PORT || 3003;
